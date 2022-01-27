6f827a78bddc1b408cd6fc66e55a3ba08ccb2ac9
Likhith Kolayari <likhith@regentmarkets.com>
2022-01-27 16:53:29 +0400

    changed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import style from './App.module.scss';
 
-function App() {
+const App=()=> {
   return (
     <div className={style.app}>
        <div className={style.start}>start</div>
