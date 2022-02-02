3ed6e917145235f614c3085691444adad42096c3
amina-deriv <amina@deriv.com>
2022-02-02 18:01:20 +0400

    form design changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById("root");
